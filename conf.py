93f968fc39e82fc181c7c5b868d7229e2049d03b
Ruchi Anand Sagar <113360969+RuchiASagar21@users.noreply.github.com>
2025-02-04 16:33:33 -0500

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,28 +1,21 @@
+# conf.py
 import os
 import sys
 
 # Add the root directory of the project to the system path
 sys.path.insert(0, os.path.abspath("."))
 
-# Detect if we're on Read the Docs
-on_rtd = os.environ.get('READTHEDOCS') == 'True'
-
 # -- Project Information -----------------------------------------------------
 project = 'cy-RTD'
 author = 'RCH'
-release = '0.1'
+release = '0.1'  # Version of the project (e.g., 0.1, 1.0.0)
 
 # -- General Configuration ---------------------------------------------------
 extensions = [
     'sphinx.ext.autodoc',        # Automatically include docstrings
     'sphinx.ext.napoleon',       # For Google-style or NumPy-style docstrings
-    'sphinx.ext.viewcode',       # Link to source code
+    'sphinx.ext.viewcode',
 ]
-
-templates_path = ['_templates']
-exclude_patterns = ['rtd-env/**', '**/*.dist-info/**', '**/site-packages/**']
-
-# -- Autodoc Options ---------------------------------------------------------
 autodoc_default_options = {
     'members': True,
     'undoc-members': True,
@@ -30,7 +23,5 @@ autodoc_default_options = {
 }
 
 # -- Options for HTML output -------------------------------------------------
-html_theme = 'sphinx_rtd_theme'
-
-# Handle static files depending on environment
-html_static_path = [] if on_rtd else ['static']
+html_theme = 'sphinx_rtd_theme'          # You can change this to 'sphinx_rtd_theme' if preferred
+html_static_path = ['static'] 
