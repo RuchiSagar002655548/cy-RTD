4aa8cf85958ed489e5af43b95d3bc85e74f9cd35
Ruchi Anand Sagar <113360969+RuchiASagar21@users.noreply.github.com>
2025-02-04 17:25:16 -0500

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,30 +1,30 @@
 import os
 import sys
 
+# Add the root directory of the project to the system path
 sys.path.insert(0, os.path.abspath("."))
 
-# Project information
+# Detect if the build is happening on Read the Docs
+on_rtd = os.environ.get('READTHEDOCS') == 'True'
+
+# -- Project Information --
 project = 'cy-RTD'
 author = 'RCH'
 release = '0.1'
 
-# General configuration
+# -- General Configuration --
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.viewcode',
+    'sphinx.ext.autodoc',        # Automatically include docstrings
+    'sphinx.ext.napoleon',       # For Google-style or NumPy-style docstrings
+    'sphinx.ext.viewcode',       # Link to source code
 ]
 
-# Paths and exclusions
 templates_path = ['_templates']
 exclude_patterns = []
 
-# Options for HTML output
+# -- Options for HTML output --
 html_theme = 'sphinx_rtd_theme'
 
-# Remove static paths if they're causing errors
-# on_rtd = os.environ.get('READTHEDOCS') == 'True'
-#if on_rtd:
- #   html_static_path = []  # Disable static paths for Read the Docs
-#else:
- #   html_static_path = ['static']
+# Handle static files: disable on RTD if it raises warnings
+html_static_path = [] if on_rtd else ['static']
+
