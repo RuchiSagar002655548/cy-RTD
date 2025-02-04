2077241602ff54012f8dfbfd46816200b62fba68
Ruchi Anand Sagar <113360969+RuchiASagar21@users.noreply.github.com>
2025-02-04 16:50:30 -0500

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,27 +1,30 @@
-# conf.py
 import os
 import sys
 
-# Add the root directory of the project to the system path
 sys.path.insert(0, os.path.abspath("."))
 
-# -- Project Information -----------------------------------------------------
+# Project information
 project = 'cy-RTD'
 author = 'RCH'
-release = '0.1'  # Version of the project (e.g., 0.1, 1.0.0)
+release = '0.1'
 
-# -- General Configuration ---------------------------------------------------
+# General configuration
 extensions = [
-    'sphinx.ext.autodoc',        # Automatically include docstrings
-    'sphinx.ext.napoleon',       # For Google-style or NumPy-style docstrings
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
     'sphinx.ext.viewcode',
 ]
-autodoc_default_options = {
-    'members': True,
-    'undoc-members': True,
-    'show-inheritance': True,
-}
 
-# -- Options for HTML output -------------------------------------------------
-html_theme = 'sphinx_rtd_theme'          # You can change this to 'sphinx_rtd_theme' if preferred
-html_static_path = ['static'] 
+# Paths and exclusions
+templates_path = ['_templates']
+exclude_patterns = []
+
+# Options for HTML output
+html_theme = 'sphinx_rtd_theme'
+
+# Remove static paths if they're causing errors
+on_rtd = os.environ.get('READTHEDOCS') == 'True'
+if on_rtd:
+    html_static_path = []  # Disable static paths for Read the Docs
+else:
+    html_static_path = ['static']
