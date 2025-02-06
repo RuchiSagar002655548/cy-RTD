303da5b9ccaeab548e6a7b81fa02c949c13fc458
Ruchi Anand Sagar <113360969+RuchiASagar21@users.noreply.github.com>
2025-02-06 13:22:48 -0500

    Update conf.py

--- conf.py
+++ conf.py
@@ -28,3 +28,13 @@ html_theme = 'sphinx_rtd_theme'
 # Handle static files: disable on RTD if it raises warnings
 html_static_path = [] if on_rtd else ['static']
 
+if not on_rtd:
+    print("Running locally. Make sure CODEARTIFACT_URL and AWS credentials are set.")
+
+# Add custom packages to sys.path for testing (if needed)
+
+custom_package_dir = os.getenv('CUSTOM_PACKAGE_PATH', None)
+if custom_package_dir:
+    sys.path.insert(0, custom_package_dir)
+
+
