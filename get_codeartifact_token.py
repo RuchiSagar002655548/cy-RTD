11c9bf0dbaf016aee9412e2df0211ddcdfea8382
Ruchi Anand Sagar <113360969+RuchiASagar21@users.noreply.github.com>
2025-02-09 12:55:24 -0500

    Create get_codeartifact_token.py

---
+++ get_codeartifact_token.py
@@ -0,0 +1,21 @@
+
+import boto3
+import os
+import subprocess
+
+# Retrieve AWS CodeArtifact token dynamically
+client = boto3.client('codeartifact', region_name=os.getenv('AWS_REGION', 'us-east-1'))
+token_response = client.get_authorization_token(
+    domain='cellarity-test',
+    domainOwner='581351078906'
+)
+codeartifact_token = token_response['authorizationToken']
+
+# Set PIP_EXTRA_INDEX_URL and install packages
+codeartifact_url = os.getenv('CODEARTIFACT_URL')
+subprocess.run([
+    'pip', 'install',
+    f'--extra-index-url={codeartifact_url}:{codeartifact_token}',
+    '--trusted-host', codeartifact_url.split("/")[2],
+    '-r', 'requirements.txt'
+])
