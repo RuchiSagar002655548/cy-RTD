4daf1397985883dcbf86ba0460edcd43d1ba4d59
ruchiasagar21 <sagar.ru@northeastern.edu>
2025-02-25 13:54:05 -0500

    updated another workflow

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="custom_py_package",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[],  # Add dependencies here
+
+    description="A custom Python package for testing AWS CodeArtifact",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/RuchiSagar002655548/cy-RTD/tree/main/custom_py_package",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
